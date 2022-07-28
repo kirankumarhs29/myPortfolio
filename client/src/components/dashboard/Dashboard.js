0caf738abea1d6d87ec36991abf75cd57960cad0
kiran kumar h s <kirankumarhs29@users.noreply.github.com>
2022-07-28 15:15:12 +0530

    backend completed

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Dashboard = (props) => {
+  return <div>Dashboard</div>;
+};
+Dashboard.protoTypes = {};
+
+export default Dashboard;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createRoot } from "react-dom/client";
+import App from "./App";
+const container = document.getElementById("root");
+const run = createRoot(container);
+run.render(<App />);
